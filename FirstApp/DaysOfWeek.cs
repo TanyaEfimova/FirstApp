75d1dc24a4c746a10825595b7c7dc6bda26dc45a
TanyaEfimova <mail5x5@mail.ru>
2021-03-11 01:05:40 +0300

    добавлен файл DaysOfWeek.cs

---
+++ FirstApp/DaysOfWeek.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApp
+{
+    enum DaysOfWeek:byte
+    {
+        Monday=1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    }
+}
