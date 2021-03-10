d4f3b0b92c384237b1ca57493b5a4e7f0d44ba13
TanyaEfimova <mail5x5@mail.ru>
2021-03-11 01:10:23 +0300

    инициализация перечисления

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,15 +6,11 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            string MyName = "Tanya";
-            byte MyAge = 42;
-            bool HaveIApet = false;
-            double MyShoeSize = 37.5;
+            DaysOfWeek MyFavoriteDay;
 
-            Console.WriteLine("My name is " + MyName);
-            Console.WriteLine("My age is " + MyAge);
-            Console.WriteLine("Do I have a pet? " + HaveIApet);
-            Console.WriteLine("My shoe size is " + MyShoeSize);
+            MyFavoriteDay = DaysOfWeek.Friday;
+
+            Console.WriteLine(MyFavoriteDay);
             Console.ReadKey();
         }
     }
