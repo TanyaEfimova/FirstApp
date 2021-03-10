3de257ce1226ae1194cc7f4dbcd327cb7f6fd605
TanyaEfimova <mail5x5@mail.ru>
2021-03-11 02:45:40 +0300

    parse to int

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -7,7 +7,7 @@ namespace FirstApp
         static void Main(string[] args)
         {
             Console.Write("Enter your age: ");
-            int age = Convert.ToInt32(Console.ReadLine());
+            int age = int.Parse(Console.ReadLine());
             Console.WriteLine("Your age is {0} ",age);
 
             Console.ReadKey();
