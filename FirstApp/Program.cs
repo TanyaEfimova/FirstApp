ef87a98749f45443e95598bdc7b5beaeec546277
TanyaEfimova <mail5x5@mail.ru>
2021-03-11 00:11:43 +0300

    Форматирование

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,6 +9,12 @@ namespace FirstApp
             const string MyName= "Jane";
 
             Console.WriteLine(MyName);
+
+            Console.WriteLine("\t Привет, мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine("\t My name is \n {0}", MyName);
+            Console.WriteLine("\u0040");
+            Console.WriteLine("\u0023");
             Console.ReadKey();
         }
     }
