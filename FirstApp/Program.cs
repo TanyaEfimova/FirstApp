6a805c2e7669ee20134d8ee42cab346f9bee2a47
TanyaEfimova <mail5x5@mail.ru>
2021-03-11 01:39:53 +0300

    ввод строки в консоль

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,19 +6,13 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            string MyName = "Татьяна";
-            Console.WriteLine("Привет, Мир");
-            Console.WriteLine();
+            string MyName = "Computer";
+            Console.WriteLine("Привет, человек");
             Console.WriteLine("Меня зовут {0}", MyName);
-            //0 — позиция нашей переменной. То есть переменная будет на позиции 0.
+            Console.Write("А как тебя зовут? ");
+            string YourName = Console.ReadLine();
+            Console.Write("Рад встрече, {0} ", YourName);
 
-            //Также мы можем вставить в строку не переменную, а строковый литерал:
-            Console.WriteLine("Меня зовут {0}", "Танюшка");
-
-            //И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
-            var age = 42;
-            var weight = 70;
-            Console.WriteLine("Возраст {0} \n Вес {1}", age, weight);
             Console.ReadKey();
         }
     }
