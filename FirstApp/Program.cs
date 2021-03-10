20a93a86aa997d59d3df2a94a3a35b9c67727a9e
TanyaEfimova <mail5x5@mail.ru>
2021-03-11 01:33:40 +0300

    вывод в консоль неск. переменных

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,7 +9,16 @@ namespace FirstApp
             string MyName = "Татьяна";
             Console.WriteLine("Привет, Мир");
             Console.WriteLine();
-            Console.WriteLine($"Меня зовут {MyName}");
+            Console.WriteLine("Меня зовут {0}", MyName);
+            //0 — позиция нашей переменной. То есть переменная будет на позиции 0.
+
+            //Также мы можем вставить в строку не переменную, а строковый литерал:
+            Console.WriteLine("Меня зовут {0}", "Танюшка");
+
+            //И, естественно, мы можем использовать несколько переменных при выводе. В этом случае располагаем их в порядке нумерации «мест».
+            var age = 42;
+            var weight = 70;
+            Console.WriteLine("Возраст {0} \n Вес {1}", age, weight);
             Console.ReadKey();
         }
     }
