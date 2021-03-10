2e0ed5b0c85627d2d816f119a771e37b328c3b37
TanyaEfimova <mail5x5@mail.ru>
2021-03-11 02:52:05 +0300

    краткая справка о нас

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,10 +6,13 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+
             Console.Write("Enter your age: ");
-            int age;
-            bool incorrect= int.TryParse(Console.ReadLine(),out age);
-            Console.WriteLine("Your age is {0} ",age);
+            int age= int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your name is {0} and age is {1} ", name, age);
 
             Console.ReadKey();
         }
