0b48fa98c7600bf637ffa6fc6382aa8c0b5e448d
TanyaEfimova <mail5x5@mail.ru>
2021-03-18 01:47:38 +0300

    добавлена обработка цвета cyan, в default - фон желтый, шрифт красный 4.1.18

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -26,11 +26,18 @@ namespace FirstApp
 					Console.WriteLine("Your color is green!");
 					break;
 
-				default:
+				case "cyan":
 					Console.BackgroundColor = ConsoleColor.Cyan;
 					Console.ForegroundColor = ConsoleColor.Black;
 
 					Console.WriteLine("Your color is cyan!");
+					break;
+
+				default:
+					Console.BackgroundColor = ConsoleColor.Yellow;
+					Console.ForegroundColor = ConsoleColor.Red;
+
+					Console.WriteLine("Your color is yellow!");
 					break;
 			}
 
