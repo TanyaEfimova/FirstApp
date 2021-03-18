8c6c617f5b142c7db3d21365d44980d6fbd1d05b
TanyaEfimova <mail5x5@mail.ru>
2021-03-18 19:33:41 +0300

    многомерные массивы

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,16 +6,10 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-			Console.WriteLine("Введите своё имя");
+            int[,] array = { { 1, 2, 3 }, { 5, 6, 7 } };
+            Console.WriteLine(array[0, 0]);
 
-			var name = Console.ReadLine();
-
-            for (int i = name.Length-1; i >= 0; i--)
-            {
-                Console.Write(name[i] + " ");
-            }
-
-			Console.ReadKey();
+            Console.ReadKey();
 		}
 	}
     
