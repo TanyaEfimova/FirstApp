f84a11e7f867d17a3df18e78f2b84edeede35f18
TanyaEfimova <mail5x5@mail.ru>
2021-03-18 19:27:03 +0300

    программа, переставляющая буквы вашего имени в обратном порядке

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,14 +10,10 @@ namespace FirstApp
 
 			var name = Console.ReadLine();
 
-			Console.WriteLine("Ваше имя по буквам: ");
-
-			foreach (var ch in name)
-			{
-				Console.Write(ch + " ");
-			}
-
-			Console.WriteLine("Последняя буква вашего имени: {0}", name[name.Length - 1]);
+            for (int i = name.Length-1; i >= 0; i--)
+            {
+                Console.Write(name[i] + " ");
+            }
 
 			Console.ReadKey();
 		}
