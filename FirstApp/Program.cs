88fd7b7d78cbcea072c7ee4bd93ba696970979b4
TanyaEfimova <mail5x5@mail.ru>
2021-03-25 02:19:20 +0300

    твои любимые цвета

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,21 +4,56 @@ namespace FirstApp
 {
     class Program
     {
-		static void Main(string[] args)
-		{
-            (string Name, string[] Dishes) User;
+        static string ShowColor()
+        {
+            Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
+            var color = Console.ReadLine();
 
-            Console.WriteLine("Введите имя");
-            User.Name = Console.ReadLine();
+            switch (color)
+            {
+                case "red":
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.ForegroundColor = ConsoleColor.Black;
 
-                User.Dishes = new string[5];
+                    Console.WriteLine("Your color is red!");
+                    break;
+
+                case "green":
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is green!");
+                    break;
+                case "cyan":
+                    Console.BackgroundColor = ConsoleColor.Cyan;
+                    Console.ForegroundColor = ConsoleColor.Black;
+
+                    Console.WriteLine("Your color is cyan!");
+                    break;
+                default:
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine("Your color is yellow!");
+                    break;
+            }
+            return color;
+        }
+        static void Main(string[] args)
+		{       
+            string[] favcolors = new string[3];
                 
-                for (int i = 0; i < User.Dishes.Length; i++)
+                for (int i = 0; i < favcolors.Length; i++)
                 {
-                Console.WriteLine("Введите любимое блюдо");
-                User.Dishes[i] = Console.ReadLine();
+                    favcolors[i] = ShowColor();
                 }
-            
+
+            Console.WriteLine("Твои любимые цвета:");
+
+                foreach(var color in favcolors)
+                {
+                    Console.WriteLine(color);
+                }
         }
 	}
     
