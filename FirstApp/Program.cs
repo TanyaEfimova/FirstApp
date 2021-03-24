fde05d623501e85c2ae1e886bc13ccef5aaf5abe
TanyaEfimova <mail5x5@mail.ru>
2021-03-25 01:01:02 +0300

    опрос о любимых блюдах

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,50 +6,19 @@ namespace FirstApp
     {
 		static void Main(string[] args)
 		{
-            (string Name, string LastName, string Login, int LoginLength, bool HasPet, string[] favcolors, double Age) User;
+            (string Name, string[] Dishes) User;
 
-            for (int k = 0; k < 3; k++)
+            Console.WriteLine("Введите имя");
+            User.Name = Console.ReadLine();
 
-            {
-
-                Console.WriteLine("Введите имя");
-                User.Name = Console.ReadLine();
-
-                Console.WriteLine("Введите фамилию");
-
-                User.LastName = Console.ReadLine();
-
-                Console.WriteLine("Введите логин");
-
-                User.Login = Console.ReadLine();
-
-                User.LoginLength = User.Login.Length;
-
-                Console.WriteLine("Есть ли у вас животные? Да или Нет");
-
-                var result = Console.ReadLine();
-
-                if (result == "Да")
+                User.Dishes = new string[5];
+                
+                for (int i = 0; i < User.Dishes.Length; i++)
                 {
-                    User.HasPet = true;
+                Console.WriteLine("Введите любимое блюдо");
+                User.Dishes[i] = Console.ReadLine();
                 }
-                else
-                {
-                    User.HasPet = false;
-                }
-
-                Console.WriteLine("Введите возраст пользователя");
-
-                User.Age = double.Parse(Console.ReadLine());
-
-                User.favcolors = new string[3];
-                Console.WriteLine("Введите три любимых цвета пользователя");
-
-                for (int i = 0; i < User.favcolors.Length; i++)
-                {
-                    User.favcolors[i] = Console.ReadLine();
-                }
-            }
+            
         }
 	}
     
