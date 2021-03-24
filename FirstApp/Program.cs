6ede463af15b3d0a5382bf616baec74a49b85fc4
TanyaEfimova <mail5x5@mail.ru>
2021-03-25 02:31:27 +0300

    методы и сортировка

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -39,21 +39,41 @@ namespace FirstApp
             }
             return color;
         }
+
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+
+            return result;
+        }
         static void Main(string[] args)
-		{       
-            string[] favcolors = new string[3];
-                
-                for (int i = 0; i < favcolors.Length; i++)
-                {
-                    favcolors[i] = ShowColor();
-                }
+		{
+            int temp;
+            int[] arr = GetArrayFromConsole();
 
-            Console.WriteLine("Твои любимые цвета:");
-
-                foreach(var color in favcolors)
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
                 {
-                    Console.WriteLine(color);
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }
                 }
+            }
+
+            foreach (var item in arr)
+            {
+                Console.Write(item +" ");
+            }
         }
 	}
     
