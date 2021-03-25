c1c84947d17cf51c1069d12d05c452dea0fa9693
TanyaEfimova <mail5x5@mail.ru>
2021-03-25 21:42:51 +0300

    добавлен метод возведения указанного числа в указанную степень

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -168,6 +168,24 @@ namespace FirstApp
                 return x * Factorial(x - 1);
             }
         }
+        static int PowerUp(int N, byte pow)
+        {
+            if (pow == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                if (pow == 1)
+                {
+                    return N;
+                }
+                else
+                {
+                    return N * PowerUp(N, --pow);
+                }
+            }
+        }
         static void Main(string[] args)
 		{
             Console.WriteLine("Напишите что-то");
