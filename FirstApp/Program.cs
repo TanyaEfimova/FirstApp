b1edd389101b229582ed67629bc3e7bf92e68fc7
TanyaEfimova <mail5x5@mail.ru>
2021-03-25 17:54:52 +0300

    метод сортировки двумерного массива

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -78,6 +78,22 @@ namespace FirstApp
 
             return array;
         }
+        static void SortComplexArray(int[,] arr)
+        {
+            int temp;
+            for (int i = 0; i <= arr.GetUpperBound(0); i++)
+            {
+                for (int j = 0; j <= arr.GetUpperBound(1); j++)
+                    for (int k = j + 1; k <= arr.GetUpperBound(1); k++)
+                        if (arr[i, j] > arr[i, k])
+                        {
+                            temp = arr[i, k];
+                            arr[i, k] = arr[i, j];
+                            arr[i, j] = temp;
+                        }
+            }
+        }
+        
         static void Main(string[] args)
 		{
             int[] numbers = new int[5];
@@ -97,7 +113,13 @@ namespace FirstApp
             {
                 Console.Write(item + " ");
             }
+
+            int[,] arr = {{ - 5, 6, 9, 1, 2, -3},{ - 8, 8, 1, 1, 2, -3}};
+
+            SortComplexArray(arr);
+
         }
+    }
 	}
     
 }
