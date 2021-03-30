6ba4c35a7438b33c07d603b104a8644dd76307ca
TanyaEfimova <mail5x5@mail.ru>
2021-03-31 02:16:20 +0300

    добавлен класс Rectangle с двумя полями, 3 конструкторами и методом для нахождения площади прямоугольника

---
+++ FirstApp/Rectangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApp
+{
+    class Rectangle
+    {
+        public int a;
+        public int b;
+
+        public int Square()
+        {
+            return a * b;
+        }
+
+        public Rectangle()
+        {
+            a = 6;
+            b = 4;
+        }
+        public Rectangle(int c)
+        {
+            a = c;
+            b = c;
+        }
+        public Rectangle(int c, int d)
+        {
+            a = c;
+            b = d;
+        }
+    }
+}
