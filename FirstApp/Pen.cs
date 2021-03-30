7fd50fcd96fb45642466a764da0a07403121e47c
TanyaEfimova <mail5x5@mail.ru>
2021-03-31 01:54:34 +0300

    добавлен класс Pen с двумя полями и двумя конструкторами

---
+++ FirstApp/Pen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstApp
+{
+    class Pen
+    {
+        public string color;
+        public int cost;
+
+        public Pen()
+        {
+            color = "Черный";
+            cost = 100;
+        }
+
+        public Pen(string penColor, int penCost)
+        {
+            color = penColor;
+            cost = penCost;
+        }
+    }
+}
