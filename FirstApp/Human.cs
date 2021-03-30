44d5022e7c0637c41b9b11065cea1e5e0766b1f9
TanyaEfimova <mail5x5@mail.ru>
2021-03-30 22:01:14 +0300

    в класс Human добавлено 3 конструктора

--- FirstApp/Human.cs
+++ FirstApp/Human.cs
@@ -15,5 +15,23 @@ namespace FirstApp
 		{
 			Console.WriteLine("Меня зовут {0}, мне {1}", name, age);
         }
+		// Конструктор 1
+		public Human()
+		{
+			name = "Неизвестно";
+			age = 20;
+		}
+		// Конструктор 2
+		public Human(string n)
+		{
+			name = n;
+			age = 20;
+		}
+		// Конструктор 3
+		public Human(string n, int a)
+		{
+			name = n;
+			age = a;
+        }
 	}
 }
