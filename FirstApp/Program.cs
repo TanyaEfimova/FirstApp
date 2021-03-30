13f243bf118fa3d01b4f0ac7e99ada4fa3f6dafc
TanyaEfimova <mail5x5@mail.ru>
2021-03-31 02:04:19 +0300

    добавлены инициализаторы

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -16,6 +16,13 @@ namespace FirstApp
             human = new Human("Дмитрий", 23);
             human.Greetings();
 
+            //инициализаторы
+            Human human2 = new Human { name = "Дима", age = 18 };
+            human2.Greetings();
+
+            Animal animal = new Animal { type = "Собака", name = "Вольт", age = 4 };
+            animal.Info();
+
             Console.ReadKey();
         }
     }
