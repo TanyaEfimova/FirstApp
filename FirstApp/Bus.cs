26a0829379d4f3755b1d69cfe5ee2aa58fbbae3e
TanyaEfimova <mail5x5@mail.ru>
2021-03-31 17:14:51 +0300

    6.3.2 добавлен класс Bus

---
+++ FirstApp/Bus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FirstApp
+{
+    class Bus
+    {
+        public int? Load;
+        public void PrintStatus()
+        {
+            if (Load.HasValue)
+            {
+                Console.WriteLine("В автобусе {0} пассажиров", Load.Value);
+            }
+            else
+            {
+                Console.WriteLine("Автобус пуст");
+            }
+        }
+    }
+}
