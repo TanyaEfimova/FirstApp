9d3cf55e8d3dffa751017aaf41ec7598520008a0
TanyaEfimova <mail5x5@mail.ru>
2021-04-07 22:22:38 +0300

    добавлен класс BaseClass и его наследник-класс InheritedClass для демонстрации работы ключевого слова base

---
+++ FirstApp/BaseClass.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstApp
+{
+    class BaseClass
+    {
+        public string Name;
+        protected string Description;
+        private int value;
+
+        public BaseClass(int value)
+        {
+            this.value = value;
+        }
+    }
+}
---
+++ FirstApp/InheritedClass.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstApp
+{
+    class InheritedClass : BaseClass
+    {
+        private int newValue;
+
+        public InheritedClass(int newValue) : base(100)
+        {
+            this.newValue = newValue;
+        }
+    }
+}
