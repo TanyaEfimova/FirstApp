573989ae27a337bc7bdd76c4087b8e54dd3a4891
TanyaEfimova <mail5x5@mail.ru>
2021-04-08 14:46:38 +0300

    в класс Obj добавлено статическое поле MaxValue

--- FirstApp/Obj.cs
+++ FirstApp/Obj.cs
@@ -9,12 +9,13 @@ namespace FirstApp
         private int length;
         private int count;
         public int Value;
+        public static int MaxValue = 2000;
 
         public Obj()
         {
             Value = 0;
         }
-        
+
         public Obj(string name, string ownerName, int objLength, int count)
         {
             this.name = name;
