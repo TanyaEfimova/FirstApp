ba165f7da1608ed84ff4923a14e9bf662c7c1f54
TanyaEfimova <mail5x5@mail.ru>
2021-04-08 05:42:59 +0300

    7.2.12

--- FirstApp/Obj.cs
+++ FirstApp/Obj.cs
@@ -8,13 +8,34 @@ namespace FirstApp
         private string owner;
         private int length;
         private int count;
+        public int Value;
 
+        public Obj(int val)
+        {
+            Value = val;
+        }
         public Obj(string name, string ownerName, int objLength, int count)
         {
             this.name = name;
             owner = ownerName;
             length = objLength;
             this.count = count;
+        }
+
+        public static Obj operator +(Obj a, Obj b)
+        {
+            return new Obj
+            {
+                Value = a.Value + b.Value
+            };
+        }
+
+        public static Obj operator -(Obj a, Obj b)
+        {
+            return new Obj
+            {
+                Value = a.Value - b.Value
+            };
         }
     }
 }
