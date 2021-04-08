3a644c7194a99861270e079dfea98f8faace1751
TanyaEfimova <mail5x5@mail.ru>
2021-04-08 14:51:30 +0300

    добавлен класс Counter со статическим свойством

---
+++ FirstApp/Counter.cs
@@ -0,0 +1,21 @@
+﻿namespace FirstApp
+{
+    class Counter
+    {
+        private static int increasingCounter;
+        public static int IncreasingCounter
+        {
+            get
+            {
+                return increasingCounter;
+            }
+            set
+            {
+                if (value > increasingCounter)
+                {
+                    increasingCounter = value;
+                }
+            }
+        }
+    }
+}
