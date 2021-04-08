1ba4f2904bb9388b03563a4962de23b065b8d896
TanyaEfimova <mail5x5@mail.ru>
2021-04-08 06:09:24 +0300

    7.2.14

---
+++ FirstApp/IndexingClass.cs
@@ -0,0 +1,24 @@
+﻿namespace FirstApp
+{
+    class IndexingClass
+    {
+        private int[] array;
+
+        public IndexingClass(int[] array)
+        {
+            this.array = array;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+    }
+}
