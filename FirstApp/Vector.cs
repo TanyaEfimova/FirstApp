e34b1dd7ae59ff99c52de8a9f717873b9fc09025
TanyaEfimova <mail5x5@mail.ru>
2021-04-08 05:06:26 +0300

    перегрузки оператора +

---
+++ FirstApp/Vector.cs
@@ -0,0 +1,34 @@
+﻿namespace FirstApp
+{
+    class Vector
+    {
+        public int X;
+        public int Y;
+
+        public static Vector operator +(Vector a, Vector b)
+        {
+            return new Vector
+            {
+                X = a.X + b.X,
+                Y = a.Y + b.Y
+            };
+        }
+
+        public static Vector operator +(Vector a)
+        {
+            return new Vector
+            {
+                X = a.X,
+                Y = a.Y
+            };
+        }
+        public static Vector operator +(Vector a, (int X, int Y) b)
+        {
+            return new Vector
+            {
+                X = a.X + b.X,
+                Y = a.Y + b.Y
+            };
+        }
+    }
+}
