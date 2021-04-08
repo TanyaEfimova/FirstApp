80bdd4c0be843a0b8637db6a109495acd42aaf5a
TanyaEfimova <mail5x5@mail.ru>
2021-04-08 14:29:12 +0300

    ассоциация, композиция и агрегация

---
+++ FirstApp/SystemUnit.cs
@@ -0,0 +1,19 @@
+﻿namespace FirstApp
+{
+    class SystemUnit
+    {
+        private MotherBoard motherBoard; //ассоциация
+
+        //композиция
+        public SystemUnit()
+        {
+            motherBoard = new MotherBoard();
+        }
+
+        //агрегация
+        public SystemUnit(MotherBoard motherBoard)
+        {
+            this.motherBoard = motherBoard;
+        }
+    }
+}
