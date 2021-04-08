623c386b8d6fc6dd9a65b914a7c6f7a2040259f4
TanyaEfimova <mail5x5@mail.ru>
2021-04-08 17:13:01 +0300

    7.6.7

--- FirstApp/Car.cs
+++ FirstApp/Car.cs
@@ -2,70 +2,13 @@
 
 namespace FirstApp
 {
-    class Car
+    class Car<T1>
     {
+        public T1 Engine;
 
-        private double Fuel;
-
-        public int Mileage;
-
-        private string color;
-
-        private TurnDirection turn;
-
-        public static int MinPrice = 100_000;
-        public static int MaxPrice;
-
-        static Car()
+        public virtual void ChangePart<T2>(T2 newPart)
         {
-            Console.WriteLine("Вызван статический конструктор класса Car");
-            MaxPrice = 1_000_000;
-        }
 
-        public Car()
-        {
-            Fuel = 50;
-            Mileage = 0;
-            color = "White";
-        }
-
-        public virtual void Move()
-        {
-            // Move a kilometer
-            Mileage++;
-            Fuel -= 0.5;
-            Console.WriteLine("Вызван метод Move класса Car");
-        }
-
-        public void FillTheCar()
-        {
-            Fuel = 50;
-        }
-
-        private void Turn(TurnDirection direction)
-        {
-            turn = direction;
-        }
-
-        public string GetColor()
-        {
-            return color;
-        }
-
-        public void ChangeColor(string newColor)
-        {
-            if (color != newColor)
-                color = newColor;
-        }
-
-        public bool IsTurningLeft()
-        {
-            return turn == TurnDirection.Left;
-        }
-
-        public bool IsTurningRight()
-        {
-            return turn == TurnDirection.Right;
         }
     }
 }
