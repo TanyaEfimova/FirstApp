a15f5937072aa2de88c6b645872a2fdd8602c2e1
TanyaEfimova <mail5x5@mail.ru>
2021-04-12 22:43:55 +0300

    Добавлен класс DiskInfo для предоставления информации об установленном в системе диске

---
+++ FirstApp/DiskInfo.cs
@@ -0,0 +1,17 @@
+﻿namespace FirstApp
+{
+    class DiskInfo
+    {
+        public string Name { get; }
+        public long Volume { get; }
+        public long FreeVolume { get; }
+
+        public DiskInfo(string name, long vol, long free)
+        {
+            Name = name;
+            Volume = vol;
+            FreeVolume = free;
+        }
+
+    }
+}
