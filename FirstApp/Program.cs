5484d50fb400b4c4991942227c0b46bee440bb70
TanyaEfimova <mail5x5@mail.ru>
2021-04-13 16:15:10 +0300

    добавлено получение имен всех файлов и папок корневого каталога

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -11,6 +11,7 @@ namespace FirstApp
             // получим системные диски
             DriveInfo[] drives = DriveInfo.GetDrives();
 
+            Console.WriteLine("Диски:");
             // Пробежимся по дискам и выведем их свойства
             foreach (DriveInfo drive in drives)
             {
@@ -23,6 +24,9 @@ namespace FirstApp
                     Console.WriteLine($"Метка: {drive.VolumeLabel}");
                 }
             }
+            Console.WriteLine();
+
+            GetCatalogs(); //   Вызов метода получения директорий
         }
         Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
 
@@ -30,6 +34,26 @@ namespace FirstApp
         {
             Folders.Add(FolderName, new Folder(FolderName));
             Console.WriteLine("Папка создана.");
+        }
+
+        static void GetCatalogs()
+        {
+            string dirName = @"/"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+            }
         }
     }
 }
