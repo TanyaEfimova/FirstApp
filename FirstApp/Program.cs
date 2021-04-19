99e8a23a9d0cfcb6c2d38ccb7dfb62ee3e261004
TanyaEfimova <mail5x5@mail.ru>
2021-04-19 23:29:33 +0300

    9.1.3

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,71 +1,15 @@
 ﻿using System;
-using System.IO;
-using System.Linq;
 
 namespace FirstApp
 {
     class Program
     {
-        public static FileInfo configFile = new FileInfo(@"SystemConfig.txt");
+
         static void Main(string[] args)
         {
-            DriveInfo[] drives = DriveInfo.GetDrives();
-            foreach (DriveInfo drive in drives.Where(d => d.DriveType == DriveType.Fixed))
-            {
-                DirectoryInfo root = drive.RootDirectory;
-                var folders = root.GetDirectories();
+            Exception except = new Exception();
 
-                Console.WriteLine($"Сканируем диск {drive.Name}...");
-
-                using (StreamWriter sw = configFile.AppendText())
-                {
-                    WriteDriveInfo(sw, drive);
-                    WriteFileInfo(sw, root);
-                    WriteFolderInfo(sw, folders);
-                }
-
-                Console.WriteLine("Завершено");
-                Console.WriteLine("_______________");
-            }
+            except.Data.Add("Дата создания исключения:", DateTime.Now);
         }
-
-
-        public static void WriteDriveInfo(StreamWriter sw, DriveInfo di)
-        {
-            sw.WriteLine($"Название: {di.Name}");
-            sw.WriteLine($"Тип: {di.DriveType}");
-            if (di.IsReady)
-            {
-                sw.WriteLine($"Объем: {di.TotalSize}");
-                sw.WriteLine($"Свободно: {di.TotalFreeSpace}");
-                sw.WriteLine($"Метка: {di.VolumeLabel}");
-            }
-        }
-
-        public static void WriteFolderInfo(StreamWriter sw, DirectoryInfo[] folders)
-        {
-            sw.WriteLine("Папки:");
-            foreach (var folder in folders)
-            {
-                sw.WriteLine(folder.Name);
-            }
-        }
-
-        public static void WriteFileInfo(StreamWriter sw, DirectoryInfo rootFolder)
-        {
-            sw.WriteLine("Файлы:");
-            foreach (var file in rootFolder.GetFiles())
-            {
-                sw.WriteLine(file.Name + $" - {file.Length} байт");
-            }
-        }
-
-
-
-
-
-
-
-
     }
 }
