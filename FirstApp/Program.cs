e2c1187c7688db9fa31c5b0cfdc39e0f23c621c2
TanyaEfimova <mail5x5@mail.ru>
2021-04-21 03:04:16 +0300

    9.3.5

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,8 +9,14 @@ namespace FirstApp
         {
             DiffDelegate deleg = IntExtensions.GetDifference;
             deleg += IntExtensions.GetSum;
+
+            Console.WriteLine("До удаления второго метода:");
             deleg.Invoke(30, 21);
 
+            deleg -= IntExtensions.GetSum;
+
+            Console.WriteLine("После удаления второго метода:");
+            deleg.Invoke(30, 21);
             Console.ReadKey();
         }
     }
