f95be20f11658e722b3280bd36b9f873e6304315
TanyaEfimova <mail5x5@mail.ru>
2021-04-28 23:42:49 +0300

    10.2.2

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -4,39 +4,24 @@ namespace FirstApp
 {
     class Program
     {
-        static ILogger Logger { get; set; }
+
         static void Main(string[] args)
         {
-            Logger = new Logger();
-
-            var worker1 = new Worker1(Logger);
-            var worker2 = new Worker2(Logger);
-            var worker3 = new Worker3(Logger);
-
-            worker1.Work();
-            worker2.Work();
-            worker3.Work();
-
-            Console.ReadKey();
+            Writer writer = new Writer();
+            ((IWriter)writer).Write();
         }
     }
 
-    public interface ICalculator
+    public interface IWriter
     {
-        void Solve(int number);
-        void Solve(int numberOne, int numberTwo);
+        void Write();
     }
 
-    public class BaseCalculator : ICalculator
+    public class Writer : IWriter
     {
-        void ICalculator.Solve(int number)
+        void IWriter.Write()
         {
-            
-        }
 
-        public void Solve(int numberOne, int numberTwo)
-        {
-            throw new NotImplementedException();
         }
     }
 }
