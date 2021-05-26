4e65bdf6d5c24f4ba758bc4dabcd5be9673cdfb0
TanyaEfimova <mail5x5@mail.ru>
2021-05-26 07:00:51 +0300

    добавлен метод сортировки пузырьком

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -35,7 +35,23 @@ namespace FirstApp
 
             }
             return -1;
+        }
 
+        static void BubbleSort(int[] array)
+        {
+            int temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
         }
     }
 }
