35a2d6f8dd5d2920a7b8bf24cb05835e717492a2
TanyaEfimova <mail5x5@mail.ru>
2021-05-31 08:39:29 +0300

    CreateMatrix

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,61 +10,9 @@ namespace FirstApp
 
         static void Main(string[] args)
         {
-            var summary = BenchmarkRunner.Run<Testing>();
-        }
+            Estimate(20);
 
-        static int BinarySearch(int value, int[] array, int left, int right)
-        {
-            while (left <= right)
-            {
-                var middle = (left + right) / 2;
-
-                var midElement = array[middle];
-
-                if (midElement == value)
-                {
-                    return middle;
-                }
-                else if (value < midElement)
-                {
-                    right = middle - 1;
-                }
-                else
-                {
-                    left = middle + 1;
-                }
-
-            }
-            return -1;
-        }
-
-        static void BubbleSort(int[] array)
-        {
-            int temp;
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                for (int j = i + 1; j < array.Length; j++)
-                {
-                    if (array[i] > array[j])
-                    {
-                        temp = array[i];
-                        array[i] = array[j];
-                        array[j] = temp;
-                    }
-                }
-            }
-        }
-
-        static int GetIndex(int[] array, int element)
-        {
-            int index;
-            for (index = 0; index < array.Length; index++)
-            {
-                if (array[index] > element)
-                    return index;
-            }
-
-            return index;
+            Console.ReadKey();
         }
 
         static void CreateMatrix(int n)
@@ -87,17 +35,9 @@ namespace FirstApp
 
         static void Estimate(int n)
         {
-            var timer = new Stopwatch();
-            timer.Start();
-
             for (int i = 0; i < n; i++)
             {
-                timer.Restart();
-
                 CreateMatrix(10000);
-
-                timer.Stop();
-                Console.WriteLine(timer.ElapsedMilliseconds);
             }
         }
 
