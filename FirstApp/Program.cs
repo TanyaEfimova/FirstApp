0718077004750f74d8a3f3735ea2aa4b4e3f9e56
TanyaEfimova <mail5x5@mail.ru>
2021-06-03 10:18:59 +0300

    13.3.11

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -9,7 +9,7 @@ namespace FirstApp
         static void Main(string[] args)
         {
             // Сохраняем предложение в строку
-            var str ="Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
+            var str = "Подсчитайте, сколько уникальных символов в этом предложении, используя HashSet<T>, учитывая знаки препинания, но не учитывая пробелы в начале и в конце предложения.";
 
             // сохраняем в массив char
             var chars = str.ToCharArray();
@@ -20,6 +20,20 @@ namespace FirstApp
                 symbols.Add(symbol);
 
             Console.WriteLine(symbols.Count);
+
+            // сохраняем знаки препинания в массив Char
+            var signs = new[] { ',', ' ', '.' };
+
+            // сохраняем числовые символы в массив Char
+            var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+            //  Проверяем, есть ли цифры
+            bool containsNumbers = symbols.Overlaps(numbers);
+            Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
+
+            // Отбрасываем знаки препинания и заново считаем
+            symbols.ExceptWith(signs);
+            Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
         }
     }
 }
