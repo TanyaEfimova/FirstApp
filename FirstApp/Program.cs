812d009f1bb598656bab0c235f2377f6ed92d263
TanyaEfimova <mail5x5@mail.ru>
2021-06-03 10:01:58 +0300

    добавлен метод ExceptWith(otherCollection)

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -45,8 +45,46 @@ namespace FirstApp
 
             foreach (var n in hSet)
                 Console.WriteLine(n);
+
+            Console.WriteLine();
+
+            // Создадим два множества
+            SortedSet<char> lettersOne = new SortedSet<char>();
+            SortedSet<char> lettersTwo = new SortedSet<char>();
+
+            //  Добавим элементы в первую
+            lettersOne.Add('A');
+            lettersOne.Add('B');
+            lettersOne.Add('C');
+            lettersOne.Add('Z');
+
+            // выведем
+            PrintCollection(lettersOne, "Первая коллекция: ");
+
+            // Добавим элементы во вторую
+            lettersTwo.Add('X');
+            lettersTwo.Add('Y');
+            lettersTwo.Add('Z');
+
+            // выведем
+            PrintCollection(lettersTwo, "Вторая коллекция");
+
+            //  Выполним вычитание множеств
+            lettersOne.ExceptWith(lettersTwo);
+
+            // Выведем результат
+            PrintCollection(lettersOne, "Результат вычитания");
+        }
+
+        static void PrintCollection(SortedSet<char> ss, string s)
+        {
+            Console.WriteLine(s);
+            foreach (char ch in ss)
+                Console.Write(ch + " ");
+            Console.WriteLine("\n");
         }
     }
 }
+
 
 
