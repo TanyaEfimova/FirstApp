9a0c60fefd8f399394c48e6f798168b702e813e6
TanyaEfimova <mail5x5@mail.ru>
2021-06-10 00:18:41 +0300

    4

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 
 namespace FirstApp
@@ -8,47 +7,26 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            // Запускаем бесконечный цикл
-            while (true)
+            string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+
+            List<string> persons = new List<string>();
+
+            foreach (var item in people)
             {
-                Console.WriteLine("Введите текст:");
-
-                // Сохраняем предложение в строку
-                var sentence = Console.ReadLine();
-                // сохраняем в массив char
-                var characters = sentence.ToCharArray();
-
-                var symbols = new HashSet<char>();
-
-                // добавляем во множество. Сохраняются только неповторяющиеся символы
-                foreach (var symbol in characters)
-                    symbols.Add(symbol);
-
-                // Выводим результат
-                Console.WriteLine($"Всего {symbols.Count} уникальных символов");
-
-                // сохраняем знаки препинания в массив Char
-                var signs = new[] { ',', ' ', '.' };
-
-                // сохраняем числовые символы в массив Char
-                var numbers = new[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-
-                //  Проверяем, есть ли цифры
-                bool containsNumbers = symbols.Overlaps(numbers);
-                Console.WriteLine($"Коллекция содержит цифры: {containsNumbers}");
-
-                // Отбрасываем знаки препинания и заново считаем
-                symbols.ExceptWith(signs);
-                Console.WriteLine($"Символов без знаков препинания:: {symbols.Count}");
-
-                Console.WriteLine();
-                Console.WriteLine();
+                if (item.ToUpper().StartsWith('А'))
+                {
+                    persons.Add(item);
+                }
             }
 
+            persons.Sort();
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person);
+            }
         }
-
     }
-
 }
 
 
