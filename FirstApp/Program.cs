8adb15f28ba98a1af9687daaab3ed7af9131dcce
TanyaEfimova <mail5x5@mail.ru>
2021-06-10 03:10:51 +0300

    тех студентов,  кто младше 28 лет и владеет английским языком

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,12 +8,36 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            string[] text = { "Раз два три", "четыре пять шесть", "семь восемь девять" };
+            // Подготовим данные
+            List<Student> students = new List<Student>
+            {
+               new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+               new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+               new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+               new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
 
-            var sequence = from str in text from word in str.Split(' ') select word;
+            // Составим запрос ()
+            var selectedStudents = students.SelectMany(
+                   // коллекция, которую нужно преобразовать
+                   s => s.Languages,
+                   // функция преобразования, применяющаяся к каждому элементу коллекции
+                   (s, l) => new { Student = s, Lang = l })
+               // дополнительные условия                          
+               .Where(s => s.Lang == "английский" && s.Student.Age < 28)
+               // указатель на объект выборки
+               .Select(s => s.Student);
 
-            foreach (var word in sequence)
-                Console.WriteLine(word);
+            // Выведем результат
+            foreach (Student student in selectedStudents)
+                Console.WriteLine($"{student.Name} - {student.Age}");
+        }
+
+        class Student
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public List<string> Languages { get; set; }
         }
     }
 }
