8b8b25c43c000e19d69d16cacb64ff94937e8253
TanyaEfimova <mail5x5@mail.ru>
2021-06-10 08:49:24 +0300

    Простая сортировка

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,47 +8,54 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-            // //  создаём пустой список с типом данных Contact
-            var phoneBook = new List<Contact>();
-
-            // добавляем контакты
-            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
-            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
-            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
-            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
-            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
-            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
-
-            // бесконечный цикл, ожидающий ввод с консоли
-            while (true)
+            // Список студентов
+            var students = new List<Student>
             {
-                // Читаем введенный с консоли символ
-                var input = Console.ReadKey().KeyChar;
+               new Student {Name="Алёна", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+               new Student {Name="Яков", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+               new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+               new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+               new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+               new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+            };
 
-                // проверяем, число ли это
-                var parsed = Int32.TryParse(input.ToString(), out int pageNumber);
+            // Сортировка по возрасту
+            var sortedStuds = from s in students orderby s.Age select s;
 
-                // если не соответствует критериям - показываем ошибку
-                if (!parsed || pageNumber < 1 || pageNumber > 3)
-                {
-                    Console.WriteLine();
-                    Console.WriteLine("Страницы не существует");
-                }
-                // если соответствует - запускаем вывод
-                else
-                {
-                    // пропускаем нужное количество элементов и берем 2 для показа на странице
-                    var pageContent = phoneBook.Skip((pageNumber - 1) * 2).Take(2);
-                    Console.WriteLine();
+            foreach (var stud in sortedStuds)
+                Console.WriteLine(stud.Name + ", " + stud.Age);
 
-                    // выводим результат
-                    foreach (var entry in pageContent)
-                        Console.WriteLine(entry.Name + " " + entry.LastName + ": " + entry.PhoneNumber);
+            Console.WriteLine();
 
-                    Console.WriteLine();
-                }
-            }
+            // Сортировка по убыванию возраста
+            var desortedStuds = from s in students orderby s.Age descending select s;
+
+            foreach (var stud in desortedStuds)
+                Console.WriteLine(stud.Name + ", " + stud.Age);
+
+            Console.WriteLine();
+
+            //  По возрастанию
+            var sortedStudentsAsc = students.OrderBy(s => s.Age);
+
+            foreach (var stud in sortedStudentsAsc)
+                Console.WriteLine(stud.Name + ", " + stud.Age);
+
+            Console.WriteLine();
+
+            //  По убыванию
+            var sortedStudentsDesc = students.OrderByDescending(s => s.Age);
+
+            foreach (var stud in sortedStudentsDesc)
+                Console.WriteLine(stud.Name + ", " + stud.Age);
         }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Languages { get; set; }
     }
 }
 
