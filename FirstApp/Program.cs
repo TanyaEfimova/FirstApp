eda3474955684119e202ba09e760b3dc36d12f35
TanyaEfimova <mail5x5@mail.ru>
2021-06-17 01:42:12 +0300

    Задание 15.2.8

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,21 +6,30 @@ namespace FirstApp
 {
     class Program
     {
+        public static List<int> listNumbers = new List<int>();
         static void Main(string[] args)
         {
-            //Простое сложение:
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Введите целое число:");
+                bool res = Int32.TryParse(Console.ReadLine(), out int number);
 
-            var simpleNumbers = new[] { 3, 5, 7, 2 };
+                if (!res)
+                {
+                    Console.WriteLine("Это не число!");
+                }
 
-            double averageNumber = Average(simpleNumbers);
-
-            Console.WriteLine(averageNumber);
-        }
-
-        static double Average(int[] numbers)
-        {
-            double result = numbers.Sum() / (double)numbers.Count();
-            return result;
+                else
+                {
+                    listNumbers.Add(number);
+                    Console.WriteLine("Количество чисел в списке: {0}", listNumbers.Count());
+                    Console.WriteLine("Сумма всех чисел списка: {0}", listNumbers.Sum());
+                    Console.WriteLine("Наибольшее число списка: {0}", listNumbers.Max());
+                    Console.WriteLine("Наименьшее число списка: {0}", listNumbers.Min());
+                    Console.WriteLine("Среднее значение чисел списка: {0}", listNumbers.Average());
+                }
+            }
         }
     }
 }
