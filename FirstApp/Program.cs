3e5a7922564c4bbb06b024e3109b0facfd661bc4
TanyaEfimova <mail5x5@mail.ru>
2021-06-16 16:45:22 +0300

    среднее арифметическое числовых объектов коллекции

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -10,35 +10,18 @@ namespace FirstApp
         {
             //Простое сложение:
 
-            var simpleNumbers = new[] { 3, 5, 7 };
+            var simpleNumbers = new[] { 3, 5, 7, 2 };
 
-            // Вернет 15
-            Console.WriteLine(simpleNumbers.Sum());
-            //-------------------------------------------------------
+            double averageNumber = Average(simpleNumbers);
 
-            //Сложный тип данных, сложение свойств:
-
-            // Список студентов
-            var students = new List<Student>
-            {
-               new Student {Name="Андрей", Age=23 },
-               new Student {Name="Сергей", Age=27 },
-               new Student {Name="Дмитрий", Age=29 }
-            };
-
-            // сумма возрастов всех студентов
-            var totalAge = students.Sum(s => s.Age);
-
-            // Вернет 79
-            Console.WriteLine(totalAge);
+            Console.WriteLine(averageNumber);
         }
-    }
 
-    class Student
-    {
-        public string Name { get; set; }
-        public int Age { get; set; }
-        public List<string> Languages { get; set; }
+        static double Average(int[] numbers)
+        {
+            double result = numbers.Sum() / (double)numbers.Count();
+            return result;
+        }
     }
 }
 
