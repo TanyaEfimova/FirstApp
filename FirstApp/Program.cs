b1667ac5bf26316b7aa4c7d1608f04e3abb3f9c3
TanyaEfimova <mail5x5@mail.ru>
2021-06-17 11:10:08 +0300

    Группировка элементов коллекций

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -6,29 +6,70 @@ namespace FirstApp
 {
     class Program
     {
-        public static List<int> listNumbers = new List<int>();
         static void Main(string[] args)
         {
-            while (true)
+            var cars = new List<Сar>()
             {
+               new Сar("Suzuki", "JP"),
+               new Сar("Toyota", "JP"),
+               new Сar("Opel", "DE"),
+               new Сar("Kamaz", "RUS"),
+               new Сar("Lada", "RUS"),
+               new Сar("Honda", "JP"),
+            };
+
+            // Группировка по стране - производителю
+            var carsByCountry = from car in cars
+                                group car by car.CountryCode; //или так: var carsByCountry = cars.GroupBy(car => car.CountryCode);
+                //с использованием проекции для создания объекта нового типа
+                var carsByCountry1 = cars
+                                   .GroupBy(car => car.CountryCode) // группируем по стране-производителю
+                                   .Select(group => new
+                                   { //  создаем новую сущность анонимного типа
+                                       Name = group.Key,
+                                       Amount = group.Count()
+                                   });
+
+            // Пробежимся по группам
+            foreach (var grouping in carsByCountry)
+            {
+                Console.WriteLine(grouping.Key + ":");
+
+                // внутри каждой группы пробежимся по элементам
+                foreach (var car in grouping)
+                    Console.WriteLine(car.Manufacturer);
+
                 Console.WriteLine();
-                Console.WriteLine("Введите целое число:");
-                bool res = Int32.TryParse(Console.ReadLine(), out int number);
+            }
+            //__________________________________________________________________________
+            // с вложенными запросами, используя ключевое слово into
+            var carsByCountry2 = from car in cars
+                                 group car by car.CountryCode into grouping // выборка в локальную переменную для вложенного запроса
+                                 select new
+                                 {
+                                     Name = grouping.Key,
+                                     Count = grouping.Count(),
+                                     Cars = from p in grouping select p //  выполним подзапрос, чтобы заполнить список машин внутри нашего нового типа
+                                 };  /* или так: var carsByCountry2 = cars
+                                                                       .GroupBy(car => car.CountryCode)
+                                                                       .Select(g => new
+                                                                       {
+                                                                           Name = g.Key,
+                                                                           Count = g.Count(),
+                                                                           Cars = g.Select(c =>c)
+                                                                       });
+                                     */
 
-                if (!res)
-                {
-                    Console.WriteLine("Это не число!");
-                }
+            // Выведем результат
+            foreach (var group in carsByCountry2)
+            {
+                // Название группы и количество элементов
+                Console.WriteLine($"{group.Name} : {group.Count} авто");
 
-                else
-                {
-                    listNumbers.Add(number);
-                    Console.WriteLine("Количество чисел в списке: {0}", listNumbers.Count());
-                    Console.WriteLine("Сумма всех чисел списка: {0}", listNumbers.Sum());
-                    Console.WriteLine("Наибольшее число списка: {0}", listNumbers.Max());
-                    Console.WriteLine("Наименьшее число списка: {0}", listNumbers.Min());
-                    Console.WriteLine("Среднее значение чисел списка: {0}", listNumbers.Average());
-                }
+                foreach (Сar car in group.Cars)
+                    Console.WriteLine(car.Manufacturer);
+
+                Console.WriteLine();
             }
         }
     }
---
+++ FirstApp/Сar.cs
@@ -0,0 +1,14 @@
+﻿namespace FirstApp
+{
+    class Сar
+    {
+            public string Manufacturer { get; set; }
+            public string CountryCode { get; set; }
+
+            public Сar(string manufacturer, string countryCode)
+            {
+                Manufacturer = manufacturer;
+                CountryCode = countryCode;
+            }
+    }
+}
