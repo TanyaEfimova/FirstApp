9bd8305e0794decf01747bad55ad6bec5f764f51
TanyaEfimova <mail5x5@mail.ru>
2021-07-01 14:02:00 +0300

    добавлены методы, возвращающие средние значения с использованием принципов KISS, DRY и YAGNI

--- FirstApp/Program.cs
+++ FirstApp/Program.cs
@@ -8,50 +8,51 @@ namespace FirstApp
     {
         static void Main(string[] args)
         {
-
+            DailyAverage();
+            WeeklyAverage();
+            MonthlyAverage();
         }
 
-        public static String GetDayOfWeek(int day)
+        private static void DailyAverage()
         {
-            if ((day < 1) || (day > 7))
-                throw new InvalidOperationException("День недели должен быть в диапазоне 1 to 7");
+            /* заменяем на код ниже
+            Console.WriteLine("Welcome to the daily forecast page");
+            Console.WriteLine($"The average temperature expects to be around {new Random().Next(10, 30)} C");
+            Console.WriteLine();
+            */
 
-            string[] days =
-            {
-                   "Monday",
-                   "Tuesday",
-                   "Wednesday",
-                   "Thursday",
-                   "Friday",
-                   "Saturday",
-                   "Sunday"
-            };
-
-            return days[day - 1];
+            Console.WriteLine($"{Constants.WelcomeString} day {Constants.LineBreak}{Constants.ForecastString} {new Random().Next(10, 30)} {Constants.LineBreak}");
         }
 
-        public static String GetDayOfWeek2(int day)
+        private static void WeeklyAverage()
         {
-            switch (day)
-            {
-                case 1:
-                    return "Monday";
-                case 2:
-                    return "Tuesday";
-                case 3:
-                    return "Wednesday";
-                case 4:
-                    return "Thursday";
-                case 5:
-                    return "Friday";
-                case 6:
-                    return "Saturday";
-                case 7:
-                    return "Sunday";
-                default:
-                    throw new InvalidOperationException("День недели должен быть в диапазоне 1 to 7");
-            }
+            /* заменяем на код ниже
+            Console.WriteLine("Welcome to the weekly forecast page");
+            Console.WriteLine($"The average temperature expects to be around {new Random().Next(10, 35)} C");
+            Console.WriteLine();
+            */
+
+            Console.WriteLine($"{Constants.WelcomeString} week {Constants.LineBreak}{Constants.ForecastString} {new Random().Next(10, 30)} {Constants.LineBreak}");
         }
+
+        private static void MonthlyAverage()
+        {
+            /* заменяем на код ниже
+            Console.WriteLine("Welcome to the monthly forecast page");
+            Console.WriteLine($"The average temperature expects to be around {new Random().Next(10, 40)} C");
+            Console.WriteLine();
+            */
+
+            Console.WriteLine($"{Constants.WelcomeString} month {Constants.LineBreak}{Constants.ForecastString} {new Random().Next(10, 30)} {Constants.LineBreak}");
+        }
+    }
+
+    public static class Constants
+    {
+        // Выносим повторяющийся код в константы
+        public const string WelcomeString = "Welcome to the forecast page for the";
+        public const string ForecastString = "The average temperature expects to be around ";
+        public const string LineBreak = "\n"; // заменяем лишние вызовы Console.Writeline на символ переноса строки
     }
 }
 
